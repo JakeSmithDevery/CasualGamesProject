7ffb25768b83619859ee313bca845940a1f0af08
Jake Smith <S00234729@atu.ie>
2025-04-24 18:15:49 +0100

    pushed changes for new branch

--- Assets/Jake/Assets/Scripts/Opponent CarAI/Waypoint.cs
+++ Assets/Jake/Assets/Scripts/Opponent CarAI/Waypoint.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Waypoint Status")]
+    public Waypoint previousWaypoint;
+    public Waypoint nextWaypoint;
 
-    // Update is called once per frame
-    void Update()
+    [Range(0f, 5f)]
+    public float waypointWidth = 5f;
+
+    public Vector3 GetPosition()
     {
-        
+        Vector3 minBound = transform.position + transform.right * waypointWidth / 2f;
+
+        Vector3 maxBound = transform.position - transform.right * waypointWidth / 2f;
+
+        return Vector3.Lerp(minBound, maxBound, Random.Range(0f, 1f));
     }
 }
